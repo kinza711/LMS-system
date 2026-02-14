e2e80f381586d278f1e89d9b678552ad125f55e4
kinza <kinzashahzadi7181@gmail.com>
2026-02-14 22:23:11 +0500

    upload cloudinary import path fixes

--- Backend/middlewares/upload.js
+++ Backend/middlewares/upload.js
@@ -59,7 +59,7 @@
 // for cloudnary production purpose
 import multer from "multer"
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
