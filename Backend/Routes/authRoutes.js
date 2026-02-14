f352226cea7cbeb933ae0fbdba35b5691c283d00
kinza <kinzashahzadi7181@gmail.com>
2026-02-14 23:41:33 +0500

    cloudinary re install

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -5,7 +5,7 @@ import {Login} from "../controllers/authController.js"
 import {Logout} from "../controllers/authController.js"
 import upload from "../middlewares/upload.js"
 
-router.post("/register", upload.single("profile") , Register);
+router.post("/register" , Register, upload.single("profile"));
 router.post("/login",  Login);
 router.get("/logout", Logout)
 
