8024a67bebe80faa50c0f6658565aa5dbe8cea55
kinza <kinzashahzadi7181@gmail.com>
2026-02-15 16:15:20 +0500

    fix: cloudinary log fixes

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -5,5 +5,8 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
+console.log("Cloudinary Key:", process.env.CLOUD_NAME);
+console.log("Cloudinary Key:", process.env.CLOUD_API_KEY);
+console.log("Cloudinary Secret:", process.env.CLOUD_API_SECRET);
 
 export default cloudinary;
